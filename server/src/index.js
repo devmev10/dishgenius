9d1f92041e3671a4b83502399182bb71e9a26224
Ravi Tokas <ravitokas90@gmail.com>
2023-11-02 07:56:10 +0530

    add dotenv

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import "dotenv/config";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect(process.env.MONGO_DB_URI);
 
 app.listen(3001, () => {
   console.log("Server Started");
