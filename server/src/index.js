73f67607647c546f6527148239dd700a36d85d17
Ravi Tokas <ravitokas90@gmail.com>
2023-11-02 08:35:33 +0530

    update server/src/index.js

--- server/src/index.js
+++ server/src/index.js
@@ -3,10 +3,14 @@ import cors from "cors";
 import mongoose from "mongoose";
 import "dotenv/config";
 
+import { userRouter } from "./routes/users";
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/auth", userRouter);
 
 mongoose.connect(process.env.MONGO_DB_URI);
 
